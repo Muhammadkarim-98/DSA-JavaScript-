ef47e91fb02c29787ef915ebbc78f6c345eea97b
Muhammadkarim <uzbekdev1998@gmail.com>
2024-01-24 11:27:15 +0500

    added some dsa

--- dsa.js
+++ dsa.js
@@ -80,3 +80,34 @@ function stars(n) {
 	return starBox;
 }
 console.log(stars(5));
+
+// Patterns about Basic Math
+//#1
+function count(n) {
+	let digits = [];
+	for (let i = 1; i <= n; i++) {
+		digits += i;
+	}
+	return digits;
+}
+
+//#2
+function countDigits(n) {
+	let digits = new String(n).split("").length;
+	return digits;
+}
+
+// #3
+function reverseNum(n) {
+	let reversed = new String(n).split("").reverse().join("");
+	return Number(reversed);
+}
+
+// #4 
+function print(n) {
+	let text = '';
+	for (let i = 0; i < n; i++) {
+		text += 'Repeated Text! '
+	}
+	return text;
+};
